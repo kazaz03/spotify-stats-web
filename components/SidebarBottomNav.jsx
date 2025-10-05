551ded7e72183bf139bb9332e18ffa8d0b18cf14
kazaz03 <akazazovic1@etf.unsa.ba>
2025-10-05 18:34:51 +0200

    responsive home page and sidebar

--- components/SidebarBottomNav.jsx
+++ components/SidebarBottomNav.jsx
@@ -1,18 +1,22 @@
 import { IoSettingsOutline } from "react-icons/io5";
 import InitialsPhoto from "/initials-pic.jpg";
 import { NavLink } from "react-router-dom";
+import useWindowWidth from "../src/hooks/useWindowWidth";
 
 export default function SidebarBottomNav() {
+  const width = useWindowWidth();
   return (
     <nav className="sidebar-nav bottom-nav">
       <div className="profile-part">
         <div className="profile-pic-container">
           <img className="profile-pic" src={InitialsPhoto}></img>
         </div>
-        <div className="profile-info-part">
-          <h3>Name Surname</h3>
-          <p>User type</p>
-        </div>
+        {width > 768 && (
+          <div className="profile-info-part">
+            <h3>Name Surname</h3>
+            <p>User type</p>
+          </div>
+        )}
       </div>
       <NavLink to="/settings">
         {({ isActive }) => (
@@ -22,13 +26,15 @@ export default function SidebarBottomNav() {
                 className={`nav-el-icon ${isActive ? "active-icon" : ""}`}
               />
             </div>
-            <span
-              className={`nav-el-link-settings ${
-                isActive ? "active-link" : ""
-              }`}
-            >
-              Settings
-            </span>
+            {width > 768 && (
+              <span
+                className={`nav-el-link-settings ${
+                  isActive ? "active-link" : ""
+                }`}
+              >
+                Settings
+              </span>
+            )}
           </div>
         )}
       </NavLink>
